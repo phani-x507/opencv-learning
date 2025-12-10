ae5986ff2b9fa9adf8543c2652b685e52d5546b5
Revenger_X507 <phani-x507@github.com>
2025-12-10 18:29:38 +0530

    Image Manipulations Done

--- OpenCVWindows.py
+++ OpenCVWindows.py
@@ -12,3 +12,19 @@ window1 = cv2.namedWindow("w1")
 cv2.imshow(window1,cb_img)
 cv2.waitKey(5000)
 cv2.destroyWindow(window1)
+
+window2 = cv2.namedWindow('w2')
+cv2.imshow(window2,coke_img)
+cv2.waitKey(0) #It acts like infinite waiting until any key pressed.
+cv2.destroyWindow(window2)
+
+window3 = cv2.namedWindow('w3')
+cv2.imshow(window3,cb_img)
+while True:
+    if cv2.waitKey(1) == ord("q"): # in this way we can assign specific key to quit windows
+        cv2.destroyWindow(window3)
+        break
+
+cv2.destroyAllWindows()
+ 
+ 
